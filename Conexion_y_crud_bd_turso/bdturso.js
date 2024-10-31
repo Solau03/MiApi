b58d26f5feaa0da9f0148146c877e9a0cb4a8bad
Laura Quina <150872507+Solau03@users.noreply.github.com>
2024-10-31 16:19:40 -0500

    v1

---
+++ Conexion_y_crud_bd_turso/bdturso.js
@@ -0,0 +1,107 @@
+import express from "express"
+import { createClient } from "@libsql/client"
+import cors from "cors"
+
+const app = express();
+const port = parseInt(process.env.PORT) || 3000;
+
+app.use(express.json());
+app.use(cors())
+const TURSO_DATABASE_URL = "libsql://try-solau-03.turso.io";
+const TURSO_AUTH_TOKEN = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzAxODgwNzcsImlkIjoiN2M5N2Y2ZGYtNGQ4My00YzM2LTg4ZWYtZjExZmI4OTBhNThlIn0.vvJksLhod3DLTFfhCsRpGwjCm6q4sPLdMypDIgnL49t7gyJqt_adSiI_5lPhApndJAdjnKEj3sqDpM_BmVc2CQ";
+
+export const turso = createClient({
+    url: TURSO_DATABASE_URL,
+    authToken: TURSO_AUTH_TOKEN
+});
+
+//listar
+app.get("/productos", async(req, res) =>{
+    const ans = await turso.execute(`SELECT * FROM productos`);
+    console.log(ans);
+    res.json( ans.rows )
+});
+//crear
+app.post("/productos", async (req, res) => {
+    const { title, description, value, image } = req.body;
+    
+    
+    try {
+      const ans = await turso.execute({
+        sql: `INSERT INTO productos (title, description, value, image) VALUES (?, ?, ?, ?)`,
+        args: [title, description, value, image]
+      });
+      res.json({
+        mensaje: "Producto creado",
+        producto: ans
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "Error al crear el producto"
+      });
+    }
+  });
+  //Eliminar
+  app.delete("/productos/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const result = await turso.execute({
+        sql: `DELETE FROM productos WHERE id= ?`,
+        args: [id]
+      });
+      console.log("Resultado de la eliminación:", result);
+      res.json({
+        mensaje: "Producto eliminado"
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "Error al eliminar el producto"
+      });
+    }
+  });
+
+//Paraeditar
+app.get("/productos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+      const ans = await turso.execute({
+          sql: "SELECT * FROM productos WHERE id = ?",
+          args: [id]
+      });
+      res.json(ans.rows[0]);
+    } catch (error) {
+      console.error("Error al obtener el producto:", error);
+      res.status(404).json({ mensaje: "Error del servidor" });
+  }
+});
+app.patch("/productos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, value, image } = req.body;
+
+  try {
+      const result = await turso.execute({
+          sql: `UPDATE productos SET title = ?, description = ?, value = ?, image = ? WHERE id = ?`,
+          args: [title, description, value, image, id]
+      });
+
+      console.log("Resultado de la actualización: ", { id, title, description, value, image });
+
+      res.json({
+          mensaje: "Los cambios se hicieron de la forma esperada",
+          result: result.rows
+      });
+  } catch (error) {
+      console.error("Error al actualizar el producto:", error);
+      res.status(404).json({
+          mensaje: "Error al actualizar el producto"
+      });
+  }
+});
+
+  
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+  })
---
+++ backend/script.js
@@ -0,0 +1,88 @@
+const link = "http://localhost:3000/productos";
+fetch(link, {
+  method: "GET"
+})
+.then(response => response.json())
+.then(data => {
+  const list = document.getElementById("productos");
+  data.forEach(productos => {
+    const myhtml = document.createRange().createContextualFragment(` 
+                    <div class="container">
+                        <div class="card">
+                          <div class="imgBx">
+                            <img src="${productos.image}" class="card-img-top" alt="...">
+                          </div>
+                          <div class="card-body">
+                            <h3 class="card-title">${productos.title}</h3>
+                            <p class="card-text">${productos.description}</p>
+                            <p class="card-text">$ ${productos.value}</p>
+                            <button onclick="editar(${productos.id})" class="btn-warning"> <img src="imagenes/pencil-square.svg" alt="editar"> Edit</button>
+                            <button onclick="deletePost(${productos.id})" class="btn-danger"> <img src="imagenes/trash3-fill.svg" alt="eliminar"> Delete</button>
+                          </div>
+                        </div>
+                      </div>
+    `);
+    list.append(myhtml); 
+  })
+});
+
+function sendForm() {
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const value = document.getElementById("value");
+    const image = document.getElementById("image");
+    const editId = document.getElementById("editId").value; 
+
+    const body = {
+        title: title.value,
+        description: description.value,
+        value: value.value,
+        image: image.value
+    };
+
+    const method = editId ? "PATCH" : "POST"; // Utilizar PATCH si se edita, en caso contrario POST
+    const url = editId ? `http://localhost:3000/productos/${editId}` : "http://localhost:3000/productos";
+
+    fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json'
+      },
+        body: JSON.stringify(body)
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log("respuesta de la api", res);
+        title.value = "";
+        description.value = "";
+        value.value = "";
+        image.value = "";
+        document.getElementById("editId").value = ""; // Restablecer el Id de edición
+        location.reload();
+    });
+}
+
+function deletePost(id) {
+    fetch(`http://localhost:3000/productos/${id}`, {
+        method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log("respuesta de la api", res);
+        location.reload();
+    });
+}
+
+function editar(id) {
+    fetch(`http://localhost:3000/productos/${id}`, {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(productos => {
+        document.getElementById("title").value = productos.title;
+        document.getElementById("description").value = productos.description;
+        document.getElementById("value").value = productos.value;
+        document.getElementById("image").value = productos.image; // Suponiendo que la primera imagen es la que hay que editar
+        document.getElementById("editId").value = productos.id; // Establecer el id de edición
+    });
+}
